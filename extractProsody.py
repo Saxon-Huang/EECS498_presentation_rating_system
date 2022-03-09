b979ec2c55410e4439e218816dee79b265be7ec7
Zechen Huang <hzechen@umich.edu>
2022-03-09 14:25:45 -0500

    extract prosody

---
+++ extractProsody.py
@@ -0,0 +1,32 @@
+import os
+from multiprocessing.dummy import Pool as ThreadPool
+
+# Set your opensmile Extracter and path here
+exe_opensmile = './opensmile/bin/SMILExtract.exe'
+path_config = './opensmile/config/prosody/prosodyShs.conf'
+
+# Set your data path and output path here
+data_path = "./wav/"
+save_path = './features/'  # output folder
+
+# Extractor set-ups
+opensmile_options = '-C ' + path_config
+
+
+def feature_extract(fn):
+    infilename = data_path + fn
+    outfilename = save_path + fn[:-4] + ".csv"
+    opensmile_call = exe_opensmile + ' ' + opensmile_options + \
+        ' -I ' + infilename + ' -csvoutput ' + outfilename
+    print(opensmile_call)
+    os.system(opensmile_call)
+
+
+for root, dirs, files in os.walk(data_path):
+    for name in files:
+        feature_extract(name)
+    # print(files)
+    # pool = ThreadPool()
+    # pool.map(feature_extract, files)
+    # pool.close()
+    # pool.join()
